417729a7d05fc2954d66488c970e89574be65740
p2r3 <p2r3@p2r3.com>
2025-08-28 13:14:09 +0300

    make apples edible

--- src/procedures.c
+++ src/procedures.c
@@ -631,6 +631,7 @@ uint8_t handlePlayerEating (PlayerData *player, uint8_t just_check) {
     case I_cooked_porkchop: food = 8; saturation = 6400; break;
     case I_cooked_mutton: food = 6; saturation = 4800; break;
     case I_rotten_flesh: food = 4; saturation = 0; break;
+    case I_apple: food = 4; saturation = 1200; break;
     default: break;
   }
 
