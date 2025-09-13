2a9e443a8dc766c291a099d0fa84cd6de0cbb25d
p2r3 <p2r3@p2r3.com>
2025-09-13 14:09:13 +0300

    fix compilation on windows

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+# Check for registries before attempting to compile, prevents confusion
 if [ ! -f "include/registries.h" ]; then
   echo "Error: 'include/registries.h' is missing."
   echo "Please follow the 'Compilation' section of the README to generate it."
   exit 1
 fi
 
-rm bareiron
-gcc src/*.c -O3 -Iinclude -o bareiron
-./bareiron
+# Figure out executable suffix (for MSYS compilation)
+case "$OSTYPE" in
+  msys*|cygwin*|win32*) exe=".exe" ;;
+  *) exe="" ;;
+esac
+
+rm -f "bareiron$exe"
+gcc src/*.c -O3 -Iinclude -o "bareiron$exe"
+"./bareiron$exe"
